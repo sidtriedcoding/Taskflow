4e538fa9c7ee417c978ab125ada7e3619867fa46
Siddharth Chaubal <siddharthchaubal@Siddharths-MacBook-Pro-2.local>
2025-10-05 01:56:51 +0530

    Minor changes to client api files

--- taskflow_client/src/state/api.ts
+++ taskflow_client/src/state/api.ts
@@ -128,7 +128,7 @@ interface CreateTeamArgs {
 
 export const api = createApi({
   baseQuery: fetchBaseQuery({
-    baseUrl: process.env.REACT_APP_API_URL,
+    baseUrl: 'http://localhost:80/api',
     prepareHeaders: (headers) => {
       headers.set('Cache-Control', 'no-cache, no-store, must-revalidate');
       headers.set('Pragma', 'no-cache');
